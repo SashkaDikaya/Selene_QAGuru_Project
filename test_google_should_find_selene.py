db78b787632b44c6f2479844eb2a75047c36e667
SashkaDikaya <dikayaav@gmail.com>
2023-03-25 14:42:58 +0300

    add fixes

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -4,15 +4,12 @@ from selenium.webdriver import ActionChains
 
 
 def test_google_search():
-    browser.config.timeout = 10
-    browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
-    browser.all('#search .g').with_(timeout=20).should(have.size_greater_than(5))
-    browser.should(have.title('selene'))
+    browser.config.hold_browser_open = True
+    browser.open('https://google.com').should(have.title('Google'))
+    browser.element('[name="q"]').should(be.blank).set_value('selene').press_enter()
+    browser.element('#search').should(have.text('yashaka/selene: User-oriented Web UI browser'))
 
-#browser.driver - вызов селениeм веб драйвер
-#ActionChains(browser.driver).click_and_hold() - вызов библиотеки ActionChains селениума, через точку можно вызывать всякие функции типа click_and_hold
-ActionChains(browser.driver).move_to_element(browser.element('#super-button').get_actual_webelement()).drag_and_drop(browser.element('#area'))
-ActionChains(browser.driver).move_to_element(browser.element('#super-button')()).drag_and_drop(browser.element('#area')()).perform()
-#() == .get_actual_webelement()
+    results = browser.all('#rso>div')
+    results.should(have.size_greater_than(6))
+
+    browser.quit()
